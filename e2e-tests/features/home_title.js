f9344343216d332755d854091e7a11b0bafd8406
Mitchell Harris <mitchell.harris2@gmail.com>
2015-05-16 11:37:12 -0500

    removed the angular template seed views and asynchronous index

--- e2e-tests/features/home_title.js
+++ e2e-tests/features/home_title.js
@@ -8,37 +8,7 @@ describe('God Has A Plan', function() {
 
   it('should automatically redirect to /home when location hash/fragment is empty', function() {
     expect(browser.getLocationAbsUrl()).toMatch("/home");
-  });
-
-
-  describe('view1', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view1');
-    });
-
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
-    });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
-  });
+  });  
   
   describe('home', function() {
 

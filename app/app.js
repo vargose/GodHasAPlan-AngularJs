4ee16f773d648ef9d5fd8749a878633a03bd15e9
Mitchell Harris <mitchell.harris2@gmail.com>
2015-05-16 11:48:01 -0500

    changed my karma config to look in components folder removed lingering references to the views created by angular seed.

--- app/app.js
+++ app/app.js
@@ -3,8 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('GodHasAPlan', [
   'ngRoute',
-  'GodHasAPlan.view1',
-  'GodHasAPlan.view2',
   'GodHasAPlan.home',
   'GodHasAPlan.version'
 ]).
